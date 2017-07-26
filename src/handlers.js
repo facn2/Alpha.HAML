76d35580c3edbc4aa0464b070982cadb980d726d
heathercoraje <yhk7830@hotmail.com>
2017-07-26 17:16:38 +0300

    add initial css file

--- src/handlers.js
+++ src/handlers.js
@@ -19,27 +19,22 @@ const findMatches = (str, callback) => {
   const filePath = path.join(__dirname, "..", "nameOfStar.txt");
   var result = [];
   fs.readFile(filePath, 'utf8', (err, file) => {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        file.toLowerCase().split(",").forEach((star) => { //split at the comma and loop through the string
-          if (star.startsWith(str) && result.length <= 10) {  //if string matches the first few inputs
-            result.push(star); //push the star name into the auto result
-          }
-        });
-        if (result.length <= 10) {
-          file.toLowerCase().split(",").forEach((star) => {
-            if ((star.indexOf(str) !== -1) && (result.length <= 10) && (result.indexOf(star) === -1)) {
-              result.push(star);
-            }
-          })
-        }
-        callback(result);
-      }
-    });
-}
+    if (err) {
+      console.log(err);
+    } else {
 
+      file.toLowerCase().split(",").forEach((star) => {
+        if ((star.indexOf(str) !== -1) && (result.length <= 10)) {
+          result.push(star);
+        }
+      });
+      // console.log(result);
+      // // return result
+      callback(result);
+      //console.log(result);
+    }
+  });
+}
 
 const handleAuto = (request, response) => {
   const str = decodeURI(request.url.split('?')[1]);
