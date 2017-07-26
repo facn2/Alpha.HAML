590a24b9fbbe4a4654ba52db7b5bf7c735e80a71
heathercoraje <yhk7830@hotmail.com>
2017-07-26 12:36:38 +0300

    add the core function autocomplete related #13

--- src/handlers.js
+++ src/handlers.js
@@ -15,22 +15,31 @@ const handleHome = (response) => {
   });
 }
 
-// const findMatches = (string) => {
-//   const filePath = path.join(__dirname, "..", "starNames.txt");
-//   fs.readFile(filePath, (err, file) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//
-//     }
-//   });
-// }
+const findMatches = (str) => {
+  const filePath = path.join(__dirname, "..", "nameOfStar.txt");
+  fs.readFile(filePath, 'utf8', (err, file) => {
+    if (err) {
+      console.log(err);
+    } else {
+      var result = [];
+      file.split(",").forEach((star) => {
+        if ((star.indexOf(str) !== -1) && (result.length <= 10)) {
+          result.push(star);
+        }
+      });
+      console.log(result);
+      // return result;
+    }
+  });
+}
 
 const handleAuto = (request, response) => {
   const str = decodeURI(request.url.split('?')[1]);
-  const match = ["king is not grumpy today"];//findMatches(str);
+  const match = findMatches(str);
   response.writeHead(200, 'Content-Type:application/json');
-  let matchObj = { "suggestions": match };
+  let matchObj = {
+    "suggestions": match
+  };
   response.end(JSON.stringify(matchObj));
 }
 
