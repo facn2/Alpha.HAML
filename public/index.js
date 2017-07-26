121e847b371aaed9bcf39e204fd5c5a0b38e5128
matthewdking <matthewdking@outlook.com>
2017-07-26 11:10:30 +0300

    send req and load new data relates #7

--- public/index.js
+++ public/index.js
@@ -1,17 +1,21 @@
-const starInput = getElementById(input-field); // get id input-field from html
+var starInput = document.getElementById('input-field'); // get id input-field from html
 starInput.addEventListener('input', function(e) { //event listener for any change in input of starInput
-  const xhr = new XMLHttpRequest(); //create new xhr request
-  xhr.onreadystatechange = function() {
-    if (xhr.readystate === 4 && xhr.status === 200) { //if status 200 and readystate complete do this
+  var inputString = e.target.value;
+  var url = `/auto?${inputString}`;
 
-    }
-    xhr.open('GET', url); //open GET request
-    xhr.send(); // send request
-  }
+  var xhr = new XMLHttpRequest(); //create new xhr request
+  xhr.open('GET', url); //open GET request
+  xhr.send(); // send request
 
   xhr.addEventListener('load', function(loadEvent) { // looking for load of data from server
-    // get data
-    // create elements
-    // append each suggestion to list
-  }
+    var listContainer = document.getElementById('starSuggestions'); //get list container
+    var starList = document.createElement('ul'); //create ul
+    var starObj = JSON.parse(loadEvent.target.responseText); //get data
+    starObj.suggestions.forEach((star) => { // for earch suggestion do this
+      var starOption = document.createElement('li'); //create li
+      starOption.innerHTML = star; // li equal to star info
+      starList.appendChild(starOption); //append li to ul
+    });
+    listContainer.appendChild(starList); // append ul to list container
+  });
 });
